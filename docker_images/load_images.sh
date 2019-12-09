1173d1d187323f62a96223a0dfb3ef79e17c82c9
Jakob Elias <jelias1@luc.edu>
2019-12-09 17:20:07 -0600

    container files added

---
+++ docker_images/load_images.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker load -i grafana.tar
+docker load -i grafana_mysqldb.tar
+docker load -i influxdb.tar
+docker load -i mdml_register.tar
+docker load -i minio.tar
+docker load -i mosquitto.tar
+docker load -i nginx.tar
+docker load -i node_red.tar
