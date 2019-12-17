82aced8e9dd7dca449fb42e461d643deeb74bca5
jelias1 <jelias1@luc.edu>
2019-12-17 14:31:10 -0600

    host var is nginx.conf and ssl certs

--- set_env.sh
+++ set_env.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-SECRETS=$(aws2 secretsmanager get-secret-value --secret-id MDML/merfpoc | jq --raw-output '.SecretString')
+export PRIVATE_KEY_PATH="/etc/pki/nginx/private/server.key"
+export CERT_PATH="/etc/pki/nginx/server.crt"
+
+SECRETS=$(aws secretsmanager get-secret-value --secret-id MDML/merfpoc | jq --raw-output '.SecretString')
 
 export MDML_INFLUXDB_SECRET=$(echo $SECRETS | jq -r '.influxdb_secret')
 export MDML_GRAFANA_SECRET=$(echo $SECRETS | jq -r '.grafana_secret')
