9b8763ba32b6874eee043e7824d2200a4cdd16fc
Jakob Elias <jelias1@luc.edu>
2021-03-10 13:01:57 -0600

    added backup script

---
+++ backup.sh
@@ -0,0 +1,21 @@
+DATE=$(date +"%m_%d_%Y")
+
+# Create folder for this backup
+mkdir backups/${DATE}
+
+# Backup Grafana Dashboards and grab the SQL file.
+docker exec -it mdml_grafana_mysqldb_1 bash -c "mysqldump -u grafana -p grafana > /var/lib/backups/mdml_graf_backup_${DATE}.sql"
+cp grafana_mysqldb/backups/mdml_graf_backup_${DATE}.sql backups/${DATE}
+
+# Grab MQTT message broker user account and ACL 
+cp mosquitto/acl_file.txt backups/${DATE}
+cp mosquitto/wordpassfile.txt backups/${DATE}
+
+# Grab object store data (.minio.sys holds user info)
+cp -r minio/mnt/data backups/${DATE}
+
+# Grab Nginx config file
+cp nginx/nginx.conf backups/${DATE}
+
+# Grab set_env file
+cp set_env.sh backups/${DATE}
